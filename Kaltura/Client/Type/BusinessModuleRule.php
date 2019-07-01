47afee10fbea8a4b2a6c54352ac9ad24dd068b6a
Backend CI <ott.rnd.core@kaltura.com>
2019-07-01 07:16:42 +0000

    Auto-generated by Jenkins

--- Kaltura/Client/Type/BusinessModuleRule.php
+++ Kaltura/Client/Type/BusinessModuleRule.php
@@ -57,7 +57,7 @@ class Kaltura_Client_Type_BusinessModuleRule extends Kaltura_Client_Type_Rule
 			if(empty($xml->actions))
 				$this->actions = array();
 			else
-				$this->actions = Kaltura_Client_ParseUtils::unmarshalArray($xml->actions, "KalturaApplyDiscountModuleAction");
+				$this->actions = Kaltura_Client_ParseUtils::unmarshalArray($xml->actions, "KalturaBusinessModuleRuleAction");
 		}
 		if(count($xml->createDate))
 			$this->createDate = (string)$xml->createDate;
@@ -74,7 +74,7 @@ class Kaltura_Client_Type_BusinessModuleRule extends Kaltura_Client_Type_Rule
 	/**
 	 * List of actions for the rule
 	 *
-	 * @var array of KalturaApplyDiscountModuleAction
+	 * @var array of KalturaBusinessModuleRuleAction
 	 */
 	public $actions;
 
--- README.txt
+++ README.txt
@@ -1,2 +1,2 @@
 Kaltura PHP API Client Library for Zend framework.
-Compatible with Kaltura server version 5.2.3.28518 and above.
+Compatible with Kaltura server version 5.2.3.43080 and above.
