140e4e4723041d252ea85a0f7ef6ecab30468ff9
Backend CI <ott.rnd.core@kaltura.com>
2020-01-08 20:05:38 +0000

    Auto-generated by Jenkins

--- Kaltura/Client/Client.php
+++ Kaltura/Client/Client.php
@@ -9,7 +9,7 @@
 // to do with audio, video, and animation what Wiki platfroms allow them to do with
 // text.
 //
-// Copyright (C) 2006-2019  Kaltura Inc.
+// Copyright (C) 2006-2020  Kaltura Inc.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
@@ -762,7 +762,7 @@ class Kaltura_Client_Client extends Kaltura_Client_ClientBase
 	{
 		parent::__construct($config);
 		
-		$this->setClientTag('php5:19-12-31');
+		$this->setClientTag('php5:20-01-08');
 		$this->setApiVersion('5.2.6.6120');
 		
 		$this->announcement = new Kaltura_Client_AnnouncementService($this);
