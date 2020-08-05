597c278f08072e8ccb90b9613ea84478f74c11ce
Backend CI <ott.rnd.core@kaltura.com>
2020-08-05 13:26:19 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/132, branch 550_BEO-8626_configurable_partial_billing_for_unified_billing

--- Kaltura/Client/Client.php
+++ Kaltura/Client/Client.php
@@ -798,8 +798,8 @@ class Kaltura_Client_Client extends Kaltura_Client_ClientBase
 	{
 		parent::__construct($config);
 		
-		$this->setClientTag('php5:20-07-30');
-		$this->setApiVersion('5.4.0.28257');
+		$this->setClientTag('php5:20-08-05');
+		$this->setApiVersion('5.4.0.28245');
 		
 		$this->announcement = new Kaltura_Client_AnnouncementService($this);
 		$this->appToken = new Kaltura_Client_AppTokenService($this);
