5b8f65c6bd3cfacacba4dee7a06e624ff15819e6
Backend CI <ott.rnd.core@kaltura.com>
2021-02-11 11:55:57 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/376, branch 6_0_4

--- Kaltura/Client/Type/CategoryTree.php
+++ Kaltura/Client/Type/CategoryTree.php
@@ -97,10 +97,6 @@ class Kaltura_Client_Type_CategoryTree extends Kaltura_Client_ObjectBase
 			$this->endDateInSeconds = (string)$xml->endDateInSeconds;
 		if(count($xml->type))
 			$this->type = (string)$xml->type;
-		if(count($xml->versionId))
-			$this->versionId = (string)$xml->versionId;
-		if(count($xml->virtualAssetId))
-			$this->virtualAssetId = (string)$xml->virtualAssetId;
 	}
 	/**
 	 * Unique identifier for the category item
@@ -182,22 +178,6 @@ class Kaltura_Client_Type_CategoryTree extends Kaltura_Client_ObjectBase
 	 * @insertonly
 	 */
 	public $type = null;
-
-	/**
-	 * Unique identifier for the category version
-	 *
-	 * @var bigint
-	 * @readonly
-	 */
-	public $versionId = null;
-
-	/**
-	 * Virtual asset id
-	 *
-	 * @var bigint
-	 * @readonly
-	 */
-	public $virtualAssetId = null;
 
 
 }
