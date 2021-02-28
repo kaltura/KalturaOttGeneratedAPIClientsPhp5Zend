6ee39f8ac5d084b1c647a5e289a8f51d6729ab2f
Backend CI <ott.rnd.core@kaltura.com>
2021-02-28 06:36:44 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/393, branch BEO-9453-epg-invalidation

--- Kaltura/Client/Client.php
+++ Kaltura/Client/Client.php
@@ -846,8 +846,8 @@ class Kaltura_Client_Client extends Kaltura_Client_ClientBase
 	{
 		parent::__construct($config);
 		
-		$this->setClientTag('php5:21-02-24');
-		$this->setApiVersion('6.1.0.28923');
+		$this->setClientTag('php5:21-02-28');
+		$this->setApiVersion('6.1.0.28931');
 		
 		$this->announcement = new Kaltura_Client_AnnouncementService($this);
 		$this->appToken = new Kaltura_Client_AppTokenService($this);
