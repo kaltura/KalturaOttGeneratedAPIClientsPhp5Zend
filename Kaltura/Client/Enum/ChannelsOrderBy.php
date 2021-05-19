1d96e39b14c35e1d10355f6874d340f65cc168e5
Backend CI <ott.rnd.core@kaltura.com>
2021-05-19 12:14:41 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/470, branch 660_BEO-9207_New_OPC_Sort_Capabilities

--- Kaltura/Client/Enum/ChannelsOrderBy.php
+++ Kaltura/Client/Enum/ChannelsOrderBy.php
@@ -38,5 +38,7 @@ class Kaltura_Client_Enum_ChannelsOrderBy extends Kaltura_Client_EnumBase
 	const NAME_DESC = "NAME_DESC";
 	const CREATE_DATE_ASC = "CREATE_DATE_ASC";
 	const CREATE_DATE_DESC = "CREATE_DATE_DESC";
+	const LAST_MODIFIED_DATE_ASC = "LAST_MODIFIED_DATE_ASC";
+	const LAST_MODIFIED_DATE_DESC = "LAST_MODIFIED_DATE_DESC";
 }
 
