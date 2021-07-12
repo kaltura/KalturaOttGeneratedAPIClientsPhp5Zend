e6217f01c0a5850898beb700bcc8844163af1e8b
Backend CI <ott.rnd.core@kaltura.com>
2021-07-12 14:30:43 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/541, branch 661_BEO-10239_Device_Mobility_Policy

--- Kaltura/Client/Client.php
+++ Kaltura/Client/Client.php
@@ -858,8 +858,8 @@ class Kaltura_Client_Client extends Kaltura_Client_ClientBase
 	{
 		parent::__construct($config);
 		
-		$this->setClientTag('php5:21-06-30');
-		$this->setApiVersion('6.5.0.29184');
+		$this->setClientTag('php5:21-07-12');
+		$this->setApiVersion('6.5.0.29190');
 		
 		$this->announcement = new Kaltura_Client_AnnouncementService($this);
 		$this->appToken = new Kaltura_Client_AppTokenService($this);
