71a26853677a15c031815e9081cec6713ddb36c8
Backend CI <ott.rnd.core@kaltura.com>
2022-05-19 12:22:55 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/899, branch BEO-11720_linear_channels_updated

---
+++ Kaltura/Client/Type/AppToken.php
@@ -0,0 +1,146 @@
+<?php
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platforms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2022  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+
+/**
+ * @package Kaltura
+ * @subpackage Client
+ */
+class Kaltura_Client_Type_AppToken extends Kaltura_Client_ObjectBase
+{
+	public function getKalturaObjectType()
+	{
+		return 'KalturaAppToken';
+	}
+	
+	public function __construct(SimpleXMLElement $xml = null)
+	{
+		parent::__construct($xml);
+		
+		if(is_null($xml))
+			return;
+		
+		if(count($xml->id))
+			$this->id = (string)$xml->id;
+		if(count($xml->expiry))
+			$this->expiry = (int)$xml->expiry;
+		if(count($xml->partnerId))
+			$this->partnerId = (int)$xml->partnerId;
+		if(count($xml->sessionDuration))
+			$this->sessionDuration = (int)$xml->sessionDuration;
+		if(count($xml->hashType))
+			$this->hashType = (string)$xml->hashType;
+		if(count($xml->sessionPrivileges))
+			$this->sessionPrivileges = (string)$xml->sessionPrivileges;
+		if(count($xml->token))
+			$this->token = (string)$xml->token;
+		if(count($xml->sessionUserId))
+			$this->sessionUserId = (string)$xml->sessionUserId;
+		if(count($xml->createDate))
+			$this->createDate = (string)$xml->createDate;
+		if(count($xml->updateDate))
+			$this->updateDate = (string)$xml->updateDate;
+	}
+	/**
+	 * The id of the application token
+	 *
+	 * @var string
+	 * @readonly
+	 */
+	public $id = null;
+
+	/**
+	 * Expiry time of current token (unix timestamp in seconds)
+	 *
+	 * @var int
+	 */
+	public $expiry = null;
+
+	/**
+	 * Partner identifier
+	 *
+	 * @var int
+	 * @readonly
+	 */
+	public $partnerId = null;
+
+	/**
+	 * Expiry duration of KS (Kaltura Session) that created using the current token (in seconds)
+	 *
+	 * @var int
+	 */
+	public $sessionDuration = null;
+
+	/**
+	 * The hash type of the token
+	 *
+	 * @var Kaltura_Client_Enum_AppTokenHashType
+	 */
+	public $hashType = null;
+
+	/**
+	 * Comma separated privileges to be applied on KS (Kaltura Session) that created using the current token
+	 *
+	 * @var string
+	 */
+	public $sessionPrivileges = null;
+
+	/**
+	 * The application token
+	 *
+	 * @var string
+	 * @readonly
+	 */
+	public $token = null;
+
+	/**
+	 * User id of KS (Kaltura Session) that created using the current token
+	 *
+	 * @var string
+	 */
+	public $sessionUserId = null;
+
+	/**
+	 * Create date
+	 *
+	 * @var bigint
+	 * @readonly
+	 */
+	public $createDate = null;
+
+	/**
+	 * Update date
+	 *
+	 * @var bigint
+	 * @readonly
+	 */
+	public $updateDate = null;
+
+
+}
+
---
+++ Kaltura/Client/UserRoleService.php
@@ -0,0 +1,118 @@
+<?php
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platforms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2022  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+
+
+/**
+ * @package Kaltura
+ * @subpackage Client
+ */
+class Kaltura_Client_UserRoleService extends Kaltura_Client_ServiceBase
+{
+	function __construct(Kaltura_Client_Client $client = null)
+	{
+		parent::__construct($client);
+	}
+
+	/**
+	 * @return Kaltura_Client_Type_UserRole
+	 * @throws Kaltura_Client_Exception|Kaltura_Client_ClientException
+	 */
+	function add(Kaltura_Client_Type_UserRole $role)
+	{
+		$kparams = array();
+		$this->client->addParam($kparams, "role", $role->toParams());
+		$this->client->queueServiceActionCall("userrole", "add", "KalturaUserRole", $kparams);
+		if ($this->client->isMultiRequest())
+			return $this->client->getMultiRequestResult();
+		$resultXml = $this->client->doQueue();
+		$resultXmlObject = new \SimpleXMLElement($resultXml);
+		$this->client->checkIfError($resultXmlObject->result);
+		$resultObject = Kaltura_Client_ParseUtils::unmarshalObject($resultXmlObject->result, "KalturaUserRole");
+		$this->client->validateObjectType($resultObject, "Kaltura_Client_Type_UserRole");
+		return $resultObject;
+	}
+
+	/**
+	 * @return bool
+	 * @throws Kaltura_Client_Exception|Kaltura_Client_ClientException
+	 */
+	function delete($id)
+	{
+		$kparams = array();
+		$this->client->addParam($kparams, "id", $id);
+		$this->client->queueServiceActionCall("userrole", "delete", null, $kparams);
+		if ($this->client->isMultiRequest())
+			return $this->client->getMultiRequestResult();
+		$resultXml = $this->client->doQueue();
+		$resultXmlObject = new \SimpleXMLElement($resultXml);
+		$this->client->checkIfError($resultXmlObject->result);
+		$resultObject = (bool)Kaltura_Client_ParseUtils::unmarshalSimpleType($resultXmlObject->result);
+		return $resultObject;
+	}
+
+	/**
+	 * @return Kaltura_Client_Type_UserRoleListResponse
+	 * @throws Kaltura_Client_Exception|Kaltura_Client_ClientException
+	 */
+	function listAction(Kaltura_Client_Type_UserRoleFilter $filter = null)
+	{
+		$kparams = array();
+		if ($filter !== null)
+			$this->client->addParam($kparams, "filter", $filter->toParams());
+		$this->client->queueServiceActionCall("userrole", "list", "KalturaUserRoleListResponse", $kparams);
+		if ($this->client->isMultiRequest())
+			return $this->client->getMultiRequestResult();
+		$resultXml = $this->client->doQueue();
+		$resultXmlObject = new \SimpleXMLElement($resultXml);
+		$this->client->checkIfError($resultXmlObject->result);
+		$resultObject = Kaltura_Client_ParseUtils::unmarshalObject($resultXmlObject->result, "KalturaUserRoleListResponse");
+		$this->client->validateObjectType($resultObject, "Kaltura_Client_Type_UserRoleListResponse");
+		return $resultObject;
+	}
+
+	/**
+	 * @return Kaltura_Client_Type_UserRole
+	 * @throws Kaltura_Client_Exception|Kaltura_Client_ClientException
+	 */
+	function update($id, Kaltura_Client_Type_UserRole $role)
+	{
+		$kparams = array();
+		$this->client->addParam($kparams, "id", $id);
+		$this->client->addParam($kparams, "role", $role->toParams());
+		$this->client->queueServiceActionCall("userrole", "update", "KalturaUserRole", $kparams);
+		if ($this->client->isMultiRequest())
+			return $this->client->getMultiRequestResult();
+		$resultXml = $this->client->doQueue();
+		$resultXmlObject = new \SimpleXMLElement($resultXml);
+		$this->client->checkIfError($resultXmlObject->result);
+		$resultObject = Kaltura_Client_ParseUtils::unmarshalObject($resultXmlObject->result, "KalturaUserRole");
+		$this->client->validateObjectType($resultObject, "Kaltura_Client_Type_UserRole");
+		return $resultObject;
+	}
+}
