036194363fe8be447fead0e179e93d8fc363cc5a
Backend CI <ott.rnd.core@kaltura.com>
2022-08-11 00:41:57 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/948, branch 791_BEO-12457_update_l2v_params

--- Kaltura/Client/Type/BasePromotion.php
+++ Kaltura/Client/Type/BasePromotion.php
@@ -6,7 +6,7 @@
 //                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
 //
 // This file is part of the Kaltura Collaborative Media Suite which allows users
-// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// to do with audio, video, and animation what Wiki platforms allow them to do with
 // text.
 //
 // Copyright (C) 2006-2022  Kaltura Inc.
@@ -26,46 +26,40 @@
 //
 // @ignore
 // ===================================================================================================
+
 /**
  * @package Kaltura
  * @subpackage Client
  */
-class Kaltura_Client_MultiRequestSubResult implements ArrayAccess
+abstract class Kaltura_Client_Type_BasePromotion extends Kaltura_Client_ObjectBase
 {
-    function __construct($value)
+	public function getKalturaObjectType()
 	{
-        $this->value = $value;
+		return 'KalturaBasePromotion';
 	}
 	
-    function __toString()
+	public function __construct(SimpleXMLElement $xml = null)
 	{
-        return '{' . $this->value . '}';
+		parent::__construct($xml);
+		
+		if(is_null($xml))
+			return;
+		
+		if(count($xml->conditions))
+		{
+			if(empty($xml->conditions))
+				$this->conditions = array();
+			else
+				$this->conditions = Kaltura_Client_ParseUtils::unmarshalArray($xml->conditions, "KalturaCondition");
+		}
 	}
+	/**
+	 * These conditions define the Promotion that applies on
+	 *
+	 * @var array of KalturaCondition
+	 */
+	public $conditions;
 
-    function __get($name)
-	{
-        return new Kaltura_Client_MultiRequestSubResult($this->value . ':' . $name);
-	}
-	
-	#[\ReturnTypeWillChange]
-	public function offsetExists($offset)
-	{
-		return true;
-	}
 
-	#[\ReturnTypeWillChange]
-	public function offsetGet($offset)
-	{
-	    return new Kaltura_Client_MultiRequestSubResult($this->value . ':' . $offset);
-	}
-
-	#[\ReturnTypeWillChange]
-	public function offsetSet($offset, $value)
-	{
-	}
-	
-	#[\ReturnTypeWillChange]
-	public function offsetUnset($offset)
-	{
-	}
 }
+
