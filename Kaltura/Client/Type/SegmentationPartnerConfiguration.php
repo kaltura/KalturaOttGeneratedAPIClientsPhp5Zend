26a962c4e018e7013cede3601d3d0bddc4181b8f
Backend CI <ott.rnd.core@kaltura.com>
2022-10-02 08:12:39 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/971, branch BEO-11915-Segmentation

--- Kaltura/Client/Type/SegmentationPartnerConfiguration.php
+++ Kaltura/Client/Type/SegmentationPartnerConfiguration.php
@@ -51,14 +51,14 @@ class Kaltura_Client_Type_SegmentationPartnerConfiguration extends Kaltura_Clien
 			$this->maxDynamicSegments = (int)$xml->maxDynamicSegments;
 	}
 	/**
-	 * The maximum number of past days to be calculated for dynamic segments
+	 * The maximum number of past days to be calculated for dynamic segments, default=180
 	 *
 	 * @var int
 	 */
 	public $maxCalculatedPeriod = null;
 
 	/**
-	 * How many dynamic segments (segments with conditions) the operator is allowed to have
+	 * How many dynamic segments (segments with conditions) the operator is allowed to have, default=50
 	 *
 	 * @var int
 	 */
