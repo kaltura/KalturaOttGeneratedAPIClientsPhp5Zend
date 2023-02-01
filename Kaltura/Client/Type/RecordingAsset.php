f117228c768edcf98cc36a4d1ce36952342b4cbb
Backend CI <ott.rnd.core@kaltura.com>
2023-02-01 13:12:40 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1053, branch 8_4_1_4

--- Kaltura/Client/Type/RecordingAsset.php
+++ Kaltura/Client/Type/RecordingAsset.php
@@ -9,7 +9,7 @@
 // to do with audio, video, and animation what Wiki platforms allow them to do with
 // text.
 //
-// Copyright (C) 2006-2022  Kaltura Inc.
+// Copyright (C) 2006-2023  Kaltura Inc.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
@@ -51,6 +51,13 @@ class Kaltura_Client_Type_RecordingAsset extends Kaltura_Client_Type_ProgramAsse
 			$this->recordingType = (string)$xml->recordingType;
 		if(count($xml->viewableUntilDate))
 			$this->viewableUntilDate = (string)$xml->viewableUntilDate;
+		if(count($xml->multiRecord))
+		{
+			if(!empty($xml->multiRecord) && ((int) $xml->multiRecord === 1 || strtolower((string)$xml->multiRecord) === 'true'))
+				$this->multiRecord = true;
+			else
+				$this->multiRecord = false;
+		}
 	}
 	/**
 	 * Recording identifier
@@ -72,6 +79,13 @@ class Kaltura_Client_Type_RecordingAsset extends Kaltura_Client_Type_ProgramAsse
 	 * @var bigint
 	 */
 	public $viewableUntilDate = null;
+
+	/**
+	 * When TRUE indicates that there are multiple KalturaImmediateRecording instances for the event.
+	 *
+	 * @var bool
+	 */
+	public $multiRecord = null;
 
 
 }
