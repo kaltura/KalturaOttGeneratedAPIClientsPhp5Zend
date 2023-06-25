7b2c4614514c51bf4f155ec59f91e6514be00a33
Backend CI <ott.rnd.core@kaltura.com>
2023-06-25 07:31:46 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1142, branch 8_9_1

--- Kaltura/Client/Type/LineupRegionalChannelFilter.php
+++ Kaltura/Client/Type/LineupRegionalChannelFilter.php
@@ -31,11 +31,11 @@
  * @package Kaltura
  * @subpackage Client
  */
-class Kaltura_Client_Type_AssetHistoryFilter extends Kaltura_Client_Type_Filter
+class Kaltura_Client_Type_LineupRegionalChannelFilter extends Kaltura_Client_Type_Filter
 {
 	public function getKalturaObjectType()
 	{
-		return 'KalturaAssetHistoryFilter';
+		return 'KalturaLineupRegionalChannelFilter';
 	}
 	
 	public function __construct(SimpleXMLElement $xml = null, $jsonObject = null)
@@ -48,65 +48,71 @@ class Kaltura_Client_Type_AssetHistoryFilter extends Kaltura_Client_Type_Filter
 		if(is_null($xml) && is_null($jsonObject))
 			return;
 		
-		if(!is_null($xml) && count($xml->typeIn))
-			$this->typeIn = (string)$xml->typeIn;
-		if(!is_null($jsonObject) && isset($jsonObject->typeIn))
-			$this->typeIn = (string)$jsonObject->typeIn;
-		if(!is_null($xml) && count($xml->assetIdIn))
-			$this->assetIdIn = (string)$xml->assetIdIn;
-		if(!is_null($jsonObject) && isset($jsonObject->assetIdIn))
-			$this->assetIdIn = (string)$jsonObject->assetIdIn;
-		if(!is_null($xml) && count($xml->statusEqual))
-			$this->statusEqual = (string)$xml->statusEqual;
-		if(!is_null($jsonObject) && isset($jsonObject->statusEqual))
-			$this->statusEqual = (string)$jsonObject->statusEqual;
-		if(!is_null($xml) && count($xml->daysLessThanOrEqual))
-			$this->daysLessThanOrEqual = (int)$xml->daysLessThanOrEqual;
-		if(!is_null($jsonObject) && isset($jsonObject->daysLessThanOrEqual))
-			$this->daysLessThanOrEqual = (int)$jsonObject->daysLessThanOrEqual;
+		if(!is_null($xml) && count($xml->regionIdEqual))
+			$this->regionIdEqual = (string)$xml->regionIdEqual;
+		if(!is_null($jsonObject) && isset($jsonObject->regionIdEqual))
+			$this->regionIdEqual = (string)$jsonObject->regionIdEqual;
+		if(!is_null($xml) && count($xml->parentRegionIncluded))
+		{
+			if(!empty($xml->parentRegionIncluded) && ((int) $xml->parentRegionIncluded === 1 || strtolower((string)$xml->parentRegionIncluded) === 'true'))
+				$this->parentRegionIncluded = true;
+			else
+				$this->parentRegionIncluded = false;
+		}
+		if(!is_null($jsonObject) && isset($jsonObject->parentRegionIncluded))
+		{
+			if(!empty($jsonObject->parentRegionIncluded) && ((int) $jsonObject->parentRegionIncluded === 1 || strtolower((string)$jsonObject->parentRegionIncluded) === 'true'))
+				$this->parentRegionIncluded = true;
+			else
+				$this->parentRegionIncluded = false;
+		}
 		if(!is_null($xml) && count($xml->kSql))
 			$this->kSql = (string)$xml->kSql;
 		if(!is_null($jsonObject) && isset($jsonObject->kSql))
 			$this->kSql = (string)$jsonObject->kSql;
+		if(!is_null($xml) && count($xml->lcnGreaterThanOrEqual))
+			$this->lcnGreaterThanOrEqual = (int)$xml->lcnGreaterThanOrEqual;
+		if(!is_null($jsonObject) && isset($jsonObject->lcnGreaterThanOrEqual))
+			$this->lcnGreaterThanOrEqual = (int)$jsonObject->lcnGreaterThanOrEqual;
+		if(!is_null($xml) && count($xml->lcnLessThanOrEqual))
+			$this->lcnLessThanOrEqual = (int)$xml->lcnLessThanOrEqual;
+		if(!is_null($jsonObject) && isset($jsonObject->lcnLessThanOrEqual))
+			$this->lcnLessThanOrEqual = (int)$jsonObject->lcnLessThanOrEqual;
 	}
 	/**
-	 * Comma separated list of asset types to search within.
-	 *             Possible values: 0 - EPG linear programs entries, any media type ID (according to media type IDs defined dynamically in the system).
-	 *             If omitted - all types should be included.
+	 * Region ID filter
 	 *
-	 * @var string
+	 * @var bigint
 	 */
-	public $typeIn = null;
+	public $regionIdEqual = null;
 
 	/**
-	 * Comma separated list of asset identifiers.
+	 * Should include lineup from parent region into response
 	 *
-	 * @var string
+	 * @var bool
 	 */
-	public $assetIdIn = null;
+	public $parentRegionIncluded = null;
 
 	/**
-	 * Which type of recently watched media to include in the result - those that finished watching, those that are in progress or both.
-	 *             If omitted or specified filter = all - return all types.
-	 *             Allowed values: progress - return medias that are in-progress, done - return medias that finished watching.
-	 *
-	 * @var Kaltura_Client_Enum_WatchStatus
-	 */
-	public $statusEqual = null;
-
-	/**
-	 * How many days back to return the watched media. If omitted, default to 7 days
-	 *
-	 * @var int
-	 */
-	public $daysLessThanOrEqual = null;
-
-	/**
-	 * KSQL expression
+	 * A valid KSQL statement - Only linear channels that satisfies the KSQL statement will be included in the results
 	 *
 	 * @var string
 	 */
 	public $kSql = null;
+
+	/**
+	 * Filter only LCNs that greater or equals to the provided number
+	 *
+	 * @var int
+	 */
+	public $lcnGreaterThanOrEqual = null;
+
+	/**
+	 * Filter only LCNs that less or equals to the provided number
+	 *
+	 * @var int
+	 */
+	public $lcnLessThanOrEqual = null;
 
 
 }
