88ac66ebe5ffe5c9e901421f2ffd67bdbd9bb13c
Backend CI <ott.rnd.core@kaltura.com>
2023-07-13 11:00:29 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1170, branch 8_9_1-java

--- Kaltura/Client/Type/VodIngestAssetResultList.php
+++ Kaltura/Client/Type/VodIngestAssetResultList.php
@@ -31,11 +31,11 @@
  * @package Kaltura
  * @subpackage Client
  */
-class Kaltura_Client_Type_RelatedFilter extends Kaltura_Client_Type_BaseSearchAssetFilter
+class Kaltura_Client_Type_VodIngestAssetResultList extends Kaltura_Client_Type_ListResponse
 {
 	public function getKalturaObjectType()
 	{
-		return 'KalturaRelatedFilter';
+		return 'KalturaVodIngestAssetResultList';
 	}
 	
 	public function __construct(SimpleXMLElement $xml = null, $jsonObject = null)
@@ -48,51 +48,27 @@ class Kaltura_Client_Type_RelatedFilter extends Kaltura_Client_Type_BaseSearchAs
 		if(is_null($xml) && is_null($jsonObject))
 			return;
 		
-		if(!is_null($xml) && count($xml->idEqual))
-			$this->idEqual = (int)$xml->idEqual;
-		if(!is_null($jsonObject) && isset($jsonObject->idEqual))
-			$this->idEqual = (int)$jsonObject->idEqual;
-		if(!is_null($xml) && count($xml->typeIn))
-			$this->typeIn = (string)$xml->typeIn;
-		if(!is_null($jsonObject) && isset($jsonObject->typeIn))
-			$this->typeIn = (string)$jsonObject->typeIn;
-		if(!is_null($xml) && count($xml->excludeWatched))
+		if(!is_null($xml) && count($xml->objects))
 		{
-			if(!empty($xml->excludeWatched) && ((int) $xml->excludeWatched === 1 || strtolower((string)$xml->excludeWatched) === 'true'))
-				$this->excludeWatched = true;
+			if(empty($xml->objects))
+				$this->objects = array();
 			else
-				$this->excludeWatched = false;
+				$this->objects = Kaltura_Client_ParseUtils::unmarshalArray($xml->objects, "KalturaVodIngestAssetResult");
 		}
-		if(!is_null($jsonObject) && isset($jsonObject->excludeWatched))
+		if(!is_null($jsonObject) && isset($jsonObject->objects))
 		{
-			if(!empty($jsonObject->excludeWatched) && ((int) $jsonObject->excludeWatched === 1 || strtolower((string)$jsonObject->excludeWatched) === 'true'))
-				$this->excludeWatched = true;
+			if(empty($jsonObject->objects))
+				$this->objects = array();
 			else
-				$this->excludeWatched = false;
+				$this->objects = Kaltura_Client_ParseUtils::jsObjectToClientObject($jsonObject->objects, "KalturaVodIngestAssetResult");
 		}
 	}
 	/**
-	 * the ID of the asset for which to return related assets
+	 * list of KalturaVodIngestAssetResult
 	 *
-	 * @var int
+	 * @var array of KalturaVodIngestAssetResult
 	 */
-	public $idEqual = null;
-
-	/**
-	 * Comma separated list of asset types to search within. 
-	 *             Possible values: any media type ID (according to media type IDs defined dynamically in the system).
-	 *             If omitted - same type as the provided asset.
-	 *
-	 * @var string
-	 */
-	public $typeIn = null;
-
-	/**
-	 * Exclude watched asset.
-	 *
-	 * @var bool
-	 */
-	public $excludeWatched = null;
+	public $objects;
 
 
 }
