5602cd2529ece4b284dc4246dd68e39592f96ec1
Backend CI <ott.rnd.core@kaltura.com>
2023-07-25 09:13:36 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1183, branch 9_0_0

--- Kaltura/Client/Client.php
+++ Kaltura/Client/Client.php
@@ -954,8 +954,8 @@ class Kaltura_Client_Client extends Kaltura_Client_ClientBase
 	{
 		parent::__construct($config);
 		
-		$this->setClientTag('php5:23-07-12');
-		$this->setApiVersion('8.9.1.30414');
+		$this->setClientTag('php5:23-07-25');
+		$this->setApiVersion('9.0.0.30430');
 		
 		$this->announcement = new Kaltura_Client_AnnouncementService($this);
 		$this->appToken = new Kaltura_Client_AppTokenService($this);
--- README.txt
+++ README.txt
@@ -1,2 +1,2 @@
 Kaltura PHP API Client Library for Zend framework.
-Compatible with Kaltura server version 8.9.1.30414 and above.
+Compatible with Kaltura server version 9.0.0.30430 and above.
