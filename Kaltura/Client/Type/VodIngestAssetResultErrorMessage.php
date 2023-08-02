252b025624f4e875340964ad17d76131170a6b7f
Backend CI <ott.rnd.core@kaltura.com>
2023-08-02 12:32:40 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1190, branch 1000_BEO-14331_region-external-id-expose

--- Kaltura/Client/Type/VodIngestAssetResultErrorMessage.php
+++ Kaltura/Client/Type/VodIngestAssetResultErrorMessage.php
@@ -31,11 +31,11 @@
  * @package Kaltura
  * @subpackage Client
  */
-class Kaltura_Client_Type_RelatedFilter extends Kaltura_Client_Type_BaseSearchAssetFilter
+class Kaltura_Client_Type_VodIngestAssetResultErrorMessage extends Kaltura_Client_ObjectBase
 {
 	public function getKalturaObjectType()
 	{
-		return 'KalturaRelatedFilter';
+		return 'KalturaVodIngestAssetResultErrorMessage';
 	}
 	
 	public function __construct(SimpleXMLElement $xml = null, $jsonObject = null)
@@ -48,51 +48,28 @@ class Kaltura_Client_Type_RelatedFilter extends Kaltura_Client_Type_BaseSearchAs
 		if(is_null($xml) && is_null($jsonObject))
 			return;
 		
-		if(!is_null($xml) && count($xml->idEqual))
-			$this->idEqual = (int)$xml->idEqual;
-		if(!is_null($jsonObject) && isset($jsonObject->idEqual))
-			$this->idEqual = (int)$jsonObject->idEqual;
-		if(!is_null($xml) && count($xml->typeIn))
-			$this->typeIn = (string)$xml->typeIn;
-		if(!is_null($jsonObject) && isset($jsonObject->typeIn))
-			$this->typeIn = (string)$jsonObject->typeIn;
-		if(!is_null($xml) && count($xml->excludeWatched))
-		{
-			if(!empty($xml->excludeWatched) && ((int) $xml->excludeWatched === 1 || strtolower((string)$xml->excludeWatched) === 'true'))
-				$this->excludeWatched = true;
-			else
-				$this->excludeWatched = false;
-		}
-		if(!is_null($jsonObject) && isset($jsonObject->excludeWatched))
-		{
-			if(!empty($jsonObject->excludeWatched) && ((int) $jsonObject->excludeWatched === 1 || strtolower((string)$jsonObject->excludeWatched) === 'true'))
-				$this->excludeWatched = true;
-			else
-				$this->excludeWatched = false;
-		}
+		if(!is_null($xml) && count($xml->message))
+			$this->message = (string)$xml->message;
+		if(!is_null($jsonObject) && isset($jsonObject->message))
+			$this->message = (string)$jsonObject->message;
+		if(!is_null($xml) && count($xml->code))
+			$this->code = (string)$xml->code;
+		if(!is_null($jsonObject) && isset($jsonObject->code))
+			$this->code = (string)$jsonObject->code;
 	}
 	/**
-	 * the ID of the asset for which to return related assets
-	 *
-	 * @var int
-	 */
-	public $idEqual = null;
-
-	/**
-	 * Comma separated list of asset types to search within. 
-	 *             Possible values: any media type ID (according to media type IDs defined dynamically in the system).
-	 *             If omitted - same type as the provided asset.
+	 * The message description with arguments place holders
 	 *
 	 * @var string
 	 */
-	public $typeIn = null;
+	public $message = null;
 
 	/**
-	 * Exclude watched asset.
+	 * The message code
 	 *
-	 * @var bool
+	 * @var string
 	 */
-	public $excludeWatched = null;
+	public $code = null;
 
 
 }
