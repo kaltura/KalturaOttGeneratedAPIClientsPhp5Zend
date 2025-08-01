8b45b8c129e3a1e2153db28d99243bccf87a4ec7
Backend CI <ott.rnd.core@kaltura.com>
2025-08-01 10:35:30 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1529, branch 11.5.0.0

--- Kaltura/Client/AssetService.php
+++ Kaltura/Client/AssetService.php
@@ -93,6 +93,31 @@ class Kaltura_Client_AssetService extends Kaltura_Client_ServiceBase
 	}
 
 	/**
+	 * @return Kaltura_Client_Type_BulkPlaybackContextResponse
+	 * @throws Kaltura_Client_Exception|Kaltura_Client_ClientException
+	 */
+	function bulkGetPlaybackContext(Kaltura_Client_Type_BulkPlaybackContextRequest $request)
+	{
+		$kparams = array();
+		$this->client->addParam($kparams, "request", $request->toParams());
+		$this->client->queueServiceActionCall("asset", "bulkGetPlaybackContext", "KalturaBulkPlaybackContextResponse", $kparams);
+		if ($this->client->isMultiRequest())
+			return $this->client->getMultiRequestResult();
+		$rawResult = $this->client->doQueue();
+		if ($this->client->getConfig()->format === Kaltura_Client_ClientBase::KALTURA_SERVICE_FORMAT_JSON) {
+			$jsObject = json_decode($rawResult);
+			$resultObject = Kaltura_Client_ParseUtils::jsObjectToClientObject($jsObject);
+			return $resultObject;
+		} else {
+			$resultXmlObject = new \SimpleXMLElement($rawResult);
+			$this->client->checkIfError($resultXmlObject->result);
+			$resultObject = Kaltura_Client_ParseUtils::unmarshalObject($resultXmlObject->result, "KalturaBulkPlaybackContextResponse");
+			$this->client->validateObjectType($resultObject, "Kaltura_Client_Type_BulkPlaybackContextResponse");
+		}
+			return $resultObject;
+	}
+
+	/**
 	 * @return Kaltura_Client_Type_AssetCount
 	 * @throws Kaltura_Client_Exception|Kaltura_Client_ClientException
 	 */
