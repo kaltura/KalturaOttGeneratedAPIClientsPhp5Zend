6c6b4cb901532c438c6845a05dcdaf54798151e8
Backend CI <ott.rnd.core@kaltura.com>
2025-08-18 14:57:29 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1540, branch 11.5.0.2

--- Kaltura/Client/Client.php
+++ Kaltura/Client/Client.php
@@ -1020,7 +1020,7 @@ class Kaltura_Client_Client extends Kaltura_Client_ClientBase
 	{
 		parent::__construct($config);
 		
-		$this->setClientTag('php5:25-08-14');
+		$this->setClientTag('php5:25-08-18');
 		$this->setApiVersion('11.5.0.2');
 		
 		$this->aiMetadataGenerator = new Kaltura_Client_AiMetadataGeneratorService($this);
